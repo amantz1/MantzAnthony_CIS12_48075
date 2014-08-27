0d12e57aa73c7d847c3273de308f97b8b582bfaf
amantz1 <amantz@student.rcc.edu>
2014-08-27 10:40:16 -0700

    Signed-off-by: amantz1 <amantz@student.rcc.edu>

---
+++ Assignments/Assignment_02/Script1.2.php
@@ -0,0 +1,22 @@
+<?php 
+	/* Anthony Mantz
+		8/27/2014
+		Assignment 2: Chapter 1 Code Snippets Script 1.2
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>First PHP</title>
+</head>
+
+<body>
+	<!--Script 1.2 - first.php -->
+    <p>This is standard HTML.</p>
+   	<?php
+	?>
+</body>
+
+</html>
---
+++ Assignments/Assignment_02/Script1.3.php
@@ -0,0 +1,23 @@
+<?php 
+	/* Anthony Mantz
+		8/27/2014
+		Assignment 2: Chapter 1 Code Snippets Script 1.3
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Second PHP</title>
+</head>
+
+<body>
+	<!--Script 1.3 - second.php -->
+    <p>This is standard HTML.</p>
+   	<?php
+		echo 'This was generated using PHP!';
+	?>
+</body>
+
+</html>
