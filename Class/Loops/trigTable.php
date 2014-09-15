eedb6a728814db494792ac94912aa16912d0d0a5
amantz1 <amantz@student.rcc.edu>
2014-09-15 09:16:27 -0700

    Signed-off-by: amantz1 <amantz@student.rcc.edu>

---
+++ Class/Loops/trigTable.php
@@ -0,0 +1,44 @@
+<?php
+	/*Anthony Mantz
+	9/15/2014
+	Purpose: First for-loop Trig Table
+	*/
+?>
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Trig Table With For Loop</title>
+</head>
+
+<body>
+<h1>Trig Table</h1>
+<table width="200" border="1">
+    <tr>
+      <th>Radians</th>
+      <th>Degrees</th>
+      <th>Sine</th>
+      <th>Cosine</th>
+      <th>Tangent</th>
+    </tr>
+<?php
+	//For - Loop
+	for($angle=0;$angle<=180;$angle+=5){
+		echo "<tr>";echo "<td>";echo "$angle";echo "</td>";
+		//Calculate
+		$rad=number_format($angle*(atan(1)/45),4);
+		$sine=number_format(sin($rad),2);
+		$cosine=number_format(cos($rad),2);
+		$tangent=number_format(tan($rad),2);
+		echo "<td>$rad</td>";
+		echo "<td>$sine</td>";
+		echo "<td>$cosine</td>";
+		echo "<td>$tangent</td>";
+		echo "</tr>";
+	}
+?>
+</table>
+
+</body>
+</html>
---
+++ Class/Loops/trigTableForm.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="trigTableWithForm.php" method="get">
+	<input name="angleStart" type="text" id="angleStart">Angle Start<br />
+    <input name="angleEnd" type="text" id="angleEnd">Angle End<br/>
+    <input name="angleInc" type="tel" id="angleInc">Angle Increment<br />
+ 	<input type="submit">  
+</form>
+
+</body>
+</html>
