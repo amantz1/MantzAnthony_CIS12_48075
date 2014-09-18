46c8e2e7d6dc57f415e11d6a2309363a1eb733f9
amantz1 <amantz@student.rcc.edu>
2014-09-18 13:30:31 -0700

    Signed-off-by: amantz1 <amantz@student.rcc.edu>

---
+++ Hmwk/Assignment3/Script2.01.php
@@ -0,0 +1,48 @@
+<?php 
+	/* Anthony Mantz
+		9/18/2014
+		Assignment 3: Chapter 2 Code Snippets Script 2.1
+	*/
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Simple HTML Form</title>
+    <style type="text/css" title="text/css" media="all">
+    label {
+        font-weight: bold;
+        color: #300ACC;
+    }
+	</style>
+</head>
+
+<body>
+<!-Script 2.1 - form.html-->
+<form action="handle_form.php" method="post">
+
+	<fieldset><legend>Enter you informaion in the form below: </legend>
+    
+    <p><label>Name: <input type="text" name="name" size="20" maxlength="40" /></label></p>
+    
+    <p><label>Email Address: <input type="text" name="email" size="40" maxlength="60" /></label></p>
+    
+    <p><label for="gender">Gender:</label><input type="radio" name="gender" value="M" /><input type="radio" name="gender" value="F" /> Female</p>
+    
+    <p><label>Age:
+    <select name="age">
+    	<option value="0-29">Under 30</option>
+        <option value="30-60">Between 30 and 60</option>
+        <option value="60+">Over 60</option>
+        </select></label></p>
+        
+  	<p><label>Comments: <textarea name="comments" rows="3" cols="40"></textarea></label></p>
+    
+    </fieldset>
+    
+    <p align="center"><input type="submit" name="submit" value="Submit My Information" /></p>
+    
+</form>
+</body>
+</html>
