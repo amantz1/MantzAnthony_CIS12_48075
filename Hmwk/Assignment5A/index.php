e84f2b95d4650f698d3f922d01c7b2da3e3a1ccf
amantz1 <amantz@student.rcc.edu>
2014-10-08 17:46:54 -0700

    signed off by amantz1 <amantz@student.rcc.edu>

--- Hmwk/Assignment5A/index.php
+++ Hmwk/Assignment5A/index.php
@@ -1,6 +1,12 @@
-<?php # Script 3.4 - index.php
+<?php # Script 3.4 and 3.7 - index.php
+
+function create_ad() {
+	echo '<p class="ad">This is an annoying ad!This is an annoying ad!This is an annoying ad!This is an annoying ad!</p>';
+}
 	$page_title = 'Welcome to this Site!';
 	include ('includes/header.html');
+	
+	create_ad();
 ?>
 <h1>Content Header</h1>
 <h2>Am I looking at the right file</h2>
@@ -10,5 +16,6 @@
     <p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
 
 <?php
+	create_ad();
 	include ('includes/footer.html');
 ?>
