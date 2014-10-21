e8ad6df795bc55b5d0e65d7a47a26ee5c1b7fcb8
amantz1 <amantz@student.rcc.edu>
2014-10-20 22:18:06 -0700

    signed off by amantz1 <amantz@student.rcc.edu>

---
+++ Projects/ClassProject/v1/cart.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>Welcome to the future home of <em>Rock and a Hard Place Productions!</em></h1>
+<p>This is the shopping cart page. It will contain a list of services for purchase and link to the checkout page.</p>
+ <?php
+	include ('includes/sitetree.php');
+?>   
+</body>
+</html>
---
+++ Projects/ClassProject/v1/clientaccount.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>Welcome to the future home of <em>Rock and a Hard Place Productions!</em></h1>
+<p>This will serve as a dashboard for the user. It will contain account profile info with options to edit and info on most recent transaction. Will link to list of past transactions.</p>
+ <?php
+	include ('includes/sitetree.php');
+?>   
+</body>
+</html>
