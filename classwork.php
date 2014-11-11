b5baa93f662acfa77edef79809d2aeaa1b665719
amantz1 <amantz@student.rcc.edu>
2014-11-10 22:59:34 -0800

    signed off by amantz1 <amantz@student.rcc.edu>

--- classwork.php
+++ classwork.php
@@ -4,13 +4,13 @@ include ('./includes/header.php');
 ?>
 <div id="nav2">
     <ul>
-        <li><a href="./class/01_HelloWorld/HelloWorld.php" target="frame">Week 1</a></li>
-        <li><a href="./class/02_VariablesStrings/VariablesStrings.php" target="frame">Week 2</a></li>
-        <li><a href="./class/03_truthTable.php/" target="frame">Week 3</a></li>
-        <li><a href="./class/04_Grade/scoreInput-2.html" target="frame">Week 4</a></li>
-        <li><a href="./class/05_Loops/DoWhile/trigTableForm.php" target="frame">Week 5</a></li>
-        <li><a href="./class/06_Arrays/1dArray/trigTableForm.php" target="frame">Week 6</a></li>
-        <li><a href="./class/07_Functions/functionExample.php" target="frame">Week 7</a></li>
+        <li><a href="./Class/01_HelloWorld/HelloWorld.php" target="frame">Week 1</a></li>
+        <li><a href="./Class/02_VariablesStrings/VariablesStrings.php" target="frame">Week 2</a></li>
+        <li><a href="./Class/03_truthTable.php/" target="frame">Week 3</a></li>
+        <li><a href="./Class/04_Grade/scoreInput-2.html" target="frame">Week 4</a></li>
+        <li><a href="./Class/05_Loops/DoWhile/trigTableForm.php" target="frame">Week 5</a></li>
+        <li><a href="./Class/06_Arrays/1dArray/trigTableForm.php" target="frame">Week 6</a></li>
+        <li><a href="./Class/07_Functions/functionExample.php" target="frame">Week 7</a></li>
     </ul>
 </div>
     <p id="framenote"><em>Please choose a link above to see what the code does or <a href="https://github.com/amantz1/MantzAnthony_CIS12_48075/tree/master/Class" target="_blank">click here</a> to see the code on github.</em></p>
--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 	//Databse connect
-	DEFINE ('DB_USER', 'root');
-	DEFINE ('DB_PASSWORD', '');
-	DEFINE ('DB_HOST', 'localhost');
-	DEFINE ('DB_NAME', 'test');
+	DEFINE ('DB_USER', '48075');
+	DEFINE ('DB_PASSWORD', '48075cis12');
+	DEFINE ('DB_HOST', '209.129.8.5');
+	DEFINE ('DB_NAME', '48075');
 	
 	$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MYSQL: ' . mysqli_connect_error() );
 	mysqli_set_charset($dbc, 'utf8');
